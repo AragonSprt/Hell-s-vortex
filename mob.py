4bf16efdc71761a58f5f73a974c4a408f317d0f5
cazou074 <142684292+cazou074@users.noreply.github.com>
2025-03-10 17:39:55 +0100

    Added a new demon "Sauron"

--- mob.py
+++ mob.py
@@ -185,12 +185,12 @@ class NPC(AnimatedSprite):
 
 
 class BallDemonNPC(NPC):
-    def __init__(self, game, path='assets/sprites/mob/ball_demon/0.png', pos=(10.5, 6.5),
-                 scale=0.7, shift=0.27, animation_time=250):
+    def __init__(self, game, path='assets/sprites/mob/Sauron/0.png', pos=(10.5, 6.5),
+                 scale=0.7, shift=0.1, animation_time=250):
         super().__init__(game, path, pos, scale, shift, animation_time)
-        self.attack_dist = 1.0
-        self.health = 150
-        self.attack_damage = 25
-        self.speed = 0.05
+        self.attack_dist = 2.0
+        self.health = 200
+        self.attack_damage = 50
+        self.speed = 0.02
         self.accuracy = 0.35
-        
+
